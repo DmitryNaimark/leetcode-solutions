839c6d3e85c3ad05c5d10c3dfd9076057f9d4d9d
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-06-22 13:58:02 +0300

    Create NPM "add-to-talbe" command, start implementing NodeJS "addToTable.js" script. It will recursively find provided Problem Folder by name and add README.md record inside the table.

---
+++ addToTable.js
@@ -0,0 +1,47 @@
+const readline = require('readline');
+const fs = require("fs");
+const replaceInFile = require('replace-in-file');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let expect = "Problem name",
+    // "771"
+    problemNumber,
+    // "Jewels and Stones"
+    problemName,
+    // "Jewels_and_Stones"
+    problemNameUnderscores,
+    // "jewels-and-stones"
+    problemNameDashed,
+    // "HashTable"
+    topicFolderName,
+    // "Jewels_and_Stones_771"
+    problemFolderName,
+    // "Jewels_and_Stones_771.js"
+    problemFileName,
+    // "HashTable/Jewels_and_Stones_771/Jewels_and_Stones_771.js"
+    problemFileFullPath;
+
+console.log(`Enter Problem name (without problem number)`);
+
+rl.on('line', (line) => {
+    // Expects "Jewels and Stones" or "Jewels_and_Stones".
+    if (expect === "Problem name") {
+        // Replace spaces with underscores.
+        problemNameUnderscores = line.replace(/ /g, '_');
+        
+        // Trim "_" from start and end.
+        problemNameUnderscores = problemNameUnderscores.replace(/^_+|_+$/g, '');
+        
+        
+    }
+});
+
+// Exits NodeJS Application.
+function exitApplication() {
+    rl.close();
+    process.exit();
+}
