60a29277947e48c174092b49924e1188e405d4b5
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-08-15 15:15:45 +0300

    [949] Remove description (according to new template)

--- topics/Array/Largest_Time_for_Given_Digits_949/[Create_array_of_all_permutations]_Largest_Time_for_Given_Digits_949.js
+++ topics/Array/Largest_Time_for_Given_Digits_949/[Create_array_of_all_permutations]_Largest_Time_for_Given_Digits_949.js
@@ -1,31 +1,9 @@
 // https://leetcode.com/problems/largest-time-for-given-digits/
 // ---------------------------------------------------
-// Given an array of 4 digits, return the largest 24 hour time that can be made.
-//
-// The smallest 24 hour time is 00:00, and the largest is 23:59.
-// Starting from 00:00, a time is larger if more time has elapsed since midnight.
-//
-// Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
-//
-// Example 1:
-//     Input: [1,2,3,4]
-//     Output: "23:41"
-//
-//
-// Example 2:
-//     Input: [5,5,5,5]
-//     Output: ""
-//
-//
-// Note:
-//     A.length == 4
-//     0 <= A[i] <= 9
-// ---------------------------------------------------
 
 // Runtime Complexity: O(4! + 4!) => O(1)
 //     There are only 4 digits. Getting all permutations is 4!, going through each of them is also 4!.
 // Space Complexity: O(4!) => O(1)
-
 /**
  * @param {number[]} a
  * @return {string}
