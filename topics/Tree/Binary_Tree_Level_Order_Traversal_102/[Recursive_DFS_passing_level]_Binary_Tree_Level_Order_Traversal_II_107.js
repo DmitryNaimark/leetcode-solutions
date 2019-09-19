834face2fa2810ddafd0a585b322117ef36ac2a6
Dmitry Naimark <dmitrynaimark@gmail.com>
2019-09-19 15:29:01 +0300

    [Add Solution] 102. Binary Tree Level Order Traversal

---
+++ topics/Tree/Binary_Tree_Level_Order_Traversal_102/[Recursive_DFS_passing_level]_Binary_Tree_Level_Order_Traversal_II_107.js
@@ -0,0 +1,88 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+// ---------------------------------------------------
+
+// Runtime Complexity: O(N)
+// Space Complexity: O(max_height), which could be O(N) for skewed Tree and O(log(N)) for balanced Tree.
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+function levelOrder(root) {
+    let res = [];
+    if (root == null) {
+        return res;
+    }
+    
+    dfs(root, 0, res);
+    
+    return res;
+}
+
+function dfs(node, level, res) {
+    if (node == null) {
+        return;
+    }
+    
+    if (level >= res.length) {
+        res.push([]);
+    }
+    
+    res[level].push(node.val);
+    dfs(node.left, level + 1, res);
+    dfs(node.right, level + 1, res);
+}
+
+// ---------------------------------------------------
+//                Uses DN functions:
+// ---------------------------------------------------
+// Creates Binary Tree from passed array of items. Item's value will be stored in .val property of a node.
+// If null is passed, then tree will store null and not a TreeNode with null value (null might occur even above the last line of nodes).
+function createBinaryTreeFromArray(arr) {
+    let rootNode = new TreeNode(arr.shift());
+    
+    let nodesInCurrentLine = [rootNode];
+    let nodesInNextLine = [];
+    
+    // Copy array, so we won't modify passed array.
+    arr = arr.slice();
+    while (arr.length > 0) {
+        while (nodesInCurrentLine.length > 0) {
+            // Skip null nodes(it could be that we have null nodes not in the last line.
+            if (nodesInCurrentLine[0] == null) {
+                nodesInCurrentLine.shift();
+                continue;
+            }
+            
+            // Create node out of the next array value and push it to "nodesInNextLine".
+            let nodeValue = arr.shift();
+            nodesInCurrentLine[0].left = (nodeValue != null)
+                ? new TreeNode(nodeValue)
+                : null;
+            
+            nodesInNextLine.push(nodesInCurrentLine[0].left);
+            
+            // Create node out of the next array value and push it to "nodesInNextLine".
+            nodeValue = arr.shift();
+            nodesInCurrentLine[0].right = (nodeValue != null)
+                ? new TreeNode(nodeValue)
+                : null;
+            nodesInNextLine.push(nodesInCurrentLine[0].right);
+            
+            nodesInCurrentLine.shift();
+        }
+        nodesInCurrentLine = nodesInNextLine;
+        nodesInNextLine = [];
+    }
+    
+    return rootNode;
+}
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+// ---------------------------------------------------
+//                    Test Cases
+// ---------------------------------------------------
+console.log(levelOrder(createBinaryTreeFromArray([3, 9, 20, null, null, 15, 7])));
